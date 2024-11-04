4b7a9c28c6453b2e37232a1c7eb1f97f7ba592e7
Callum Luxford <151152271+Callum-Luxford@users.noreply.github.com>
2024-11-04 17:05:05 +0000

    fourth commit, additions to the categories page

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -41,6 +41,25 @@ exports.exploreCategories = async (req, res) => {
   }
 };
 
+// GET /categories/:id
+// Categories by Id
+exports.exploreCategoriesById = async (req, res) => {
+  try {
+    let categoryId = req.params.id;
+
+    const limitNumber = 20;
+    const categoryById = await Recipe.find({ 'category': categoryId }).limit(
+      limitNumber
+    );
+    res.render("categories", {
+      title: "Categories",
+      categoryById,
+    });
+  } catch (error) {
+    res.status(500).send({ message: error.message || "Error Occured" });
+  }
+};
+
 // GET /recipe/:id
 // Recipe
 exports.exploreRecipe = async (req, res) => {
--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -7,8 +7,9 @@ const router = express.Router();
 
 // APP ROUTES
 router.get("/", recipeController.homePage);
-router.get("/categories", recipeController.exploreCategories);
 router.get("/recipe/:id", recipeController.exploreRecipe);
+router.get("/categories", recipeController.exploreCategories);
+router.get("/categories/:id", recipeController.exploreCategoriesById);
 
 // EXPORT MODULE
 module.exports = router;
